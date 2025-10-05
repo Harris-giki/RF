849377a11e7a1ad3668ab923269a1390764ab2b3
Muhammad Haris <harris.giki@gmail.com>
2025-10-06 00:59:41 +0500

    Cleared landing page

--- components/welcome.tsx
+++ components/welcome.tsx
@@ -57,10 +57,6 @@ export const Welcome = ({
           </span>{" "}
           .
         </p>
-        <p>
-          AI powered Telephony/Web Voice AI Agents&apos; features are designed
-          to enhance your business&apos;s communication.
-        </p>
       </footer>
     </section>
   );
